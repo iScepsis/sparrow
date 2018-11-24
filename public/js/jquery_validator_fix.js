8e2a6b79f4af4424d27da2907cb08d530dbdba3b
iScepsis <fed_smolensk@mail.ru>
2018-11-24 23:20:31 +0300

    Фикс валидатора

---
+++ public/js/jquery_validator_fix.js
@@ -0,0 +1,17 @@
+/**
+ * Фикс jquery-validator для работы c bootstrap 4
+ */
+
+$.validator.setDefaults({
+    errorElement: 'span',
+    errorPlacement: function (error, element) {
+        error.addClass('invalid-feedback');
+        element.closest('.form-group').append(error);
+    },
+    highlight: function (element, errorClass, validClass) {
+        $(element).addClass('is-invalid');
+    },
+    unhighlight: function (element, errorClass, validClass) {
+        $(element).removeClass('is-invalid');
+    }
+});
--- public/js/routes/registry.js
+++ public/js/routes/registry.js
@@ -1,16 +1,47 @@
 $(document).ready(function(){
 
+
+
+    $('#registryForm').validate({
+        rules: {
+            username: {
+                required: true,
+                minlength: 8,
+                maxlength: 16
+            },
+            password: {
+                required: true,
+                minlength: 6,
+                maxlength: 16
+            },
+
+        },
+        messages: {
+            username: {
+                required: "Логин пользователя необходим для идентификации в системе",
+                minlength: $.validator.format("Логин должен содержать как минимум {0} символов!"),
+                maxlength: $.validator.format("Логин должен содержать не более {0} символов!"),
+            },
+            password: {
+                required: "Пароль обязателен для заполнения",
+                minlength: $.validator.format("Пароль должен содержать как минимум {0} символов!"),
+                maxlength: $.validator.format("Пароль должен содержать не более {0} символов!"),
+            },
+        }
+    });
+
     $('#registryForm').on('submit', function(e){
         e.preventDefault();
 
         $.ajax({
             url: 'registry',
             method: 'post',
+            dataType: json,
             data: {
-                email: $('#userMail').val(),
-                username: $('#userLogin').val(),
-                password: $('#userPass').val(),
-                passwordRep: $('#userPassRepeat').val()
+                email: $('#email').val(),
+                username: $('#username').val(),
+                password: $('#password').val(),
+                passwordRepeat: $('#passwordRepeat').val()
             },
             success: function (res) {
                 console.log(res);
