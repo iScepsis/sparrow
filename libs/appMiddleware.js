712b0c174228517974528b5beff8a74fcbe5349c
scepsis <f.senin@nextcontact.su>
2018-12-17 17:55:50 +0300

    Супер изменения

--- libs/appMiddleware.js
+++ libs/appMiddleware.js
@@ -1,6 +1,10 @@
 var appMiddleware = function (req, res, next) {
     res.locals.isAuthenticated = req.isAuthenticated();
+    res.locals.user = {};
+    res.locals.user.username = req.user.username;
+    res.locals.user.email = req.user.email;
+    res.locals.user.id = req.user._id;
     next();
-}
+};
 
 module.exports = appMiddleware;
--- libs/chat.js
+++ libs/chat.js
@@ -2,10 +2,17 @@ module.exports = function(server) {
     var io = require('socket.io')(server);
 
     io.on('connection', function(socket){
+
         socket.on('chat message', function(msg){
             io.emit('chat message', msg);
         });
+
+        socket.on('set-login', function(login) {
+            socket.login = login;
+            console.log(socket.login);
+        });
     });
+
 
 
     return io;
